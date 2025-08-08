fcaa638e59571a04d8d0a06823497b2369442b64
Adityahub404singh <singhaditya4560@gmail.com>
2025-08-08 17:14:25 +0530

    2nd

--- workspace/server/vite.ts
+++ workspace/server/vite.ts
@@ -3,7 +3,7 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 
-import { createServer as createViteServer, type ViteDevServer } from "vite";
+import { createServer as createViteServer, type ViteDevServer} from "vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
